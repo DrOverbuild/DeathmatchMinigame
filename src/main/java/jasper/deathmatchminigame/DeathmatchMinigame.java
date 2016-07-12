d57731d4fce806803bae8e99b57cbe1f4e2b5a7f
Jasper Reddin <jazzbeenjamin@me.com>
2016-07-12 17:53:17 -0500

    Set up project

---
+++ src/main/java/jasper/deathmatchminigame/DeathmatchMinigame.java
@@ -0,0 +1,9 @@
+package jasper.deathmatchminigame;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by jasper on 7/12/16.
+ */
+public class DeathmatchMinigame extends JavaPlugin {
+}
